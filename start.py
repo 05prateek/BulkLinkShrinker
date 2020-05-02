0ae38ae0ad37ae7cc0f51f41e4293d62353a6e19
05prateek <47900405+05prateek@users.noreply.github.com>
2020-05-03 01:04:51 +0530

    Update start.py

--- start.py
+++ start.py
@@ -6,7 +6,7 @@ with open('input/input.txt', mode='r',encoding='utf8') as f:
 f.close()
 fo=open('output/shrunked.txt','w',encoding='utf8')
 for i in inn:
-    r =requests.get('https://shrinkme.io/api?api=322fa5ea4e09eee44e178a3ea9e518d588d3d16a&url='+i+'&format=text')
+    r =requests.get('https://oke.io/api/?api=a9bf4fa648573dc138b33263f1890914001a0ed8&url='+i+'&format=text')
     print(c," ",i,"\n")
     fo.write(r.text +"\n")
     c+=1
